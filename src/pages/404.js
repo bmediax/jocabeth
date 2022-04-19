90039fd938b8c9d689dee236e741ab2e21347adb
Bmediax <hello@bmediax.dev>
2022-04-18 17:07:29 -0700

    Page Template

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,12 @@
-import * as React from "react"
+import * as React from "react";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout";
+import Seo from "../components/seo";
 
 const NotFoundPage = () => (
-  <Layout>
-    <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+  <div style={{ textAlign: "center", fontSize: "2rem", lineHeight: "1.5" }}>
+    Cannot find what you are looking for...
+  </div>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
